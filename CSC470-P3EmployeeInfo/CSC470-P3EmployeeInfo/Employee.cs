9d9adc7422d9d339e486051654b2cc25ae9b57e6
Taner Tharp <tharptanner@gmail.com>
2020-09-15 23:00:39 -0500

    Added basic parameters to Employee.cs, Created and added IEmployeeRepository.cs interface, as well as 3 lines of code. L1: save employee L2: get list of employees, L3: get salary via employee ID.

---
+++ CSC470-P3EmployeeInfo/CSC470-P3EmployeeInfo/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_P3EmployeeInfo
+{
+    public class Employee
+    {
+        public int Id;
+        public string LastName;
+        public string FirstName;
+        public decimal Salary;
+    }
+}
