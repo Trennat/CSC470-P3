1cd996c3d7b5522a53ef64dc67e415dc8c7aa552
Taner Tharp <tharptanner@gmail.com>
2020-09-17 23:36:27 -0500

    add tester GetSalary

---
+++ CSC470-P3EmployeeInfo/UnitTestFakeEmployeeRepository/GetSalary.cs
@@ -0,0 +1,26 @@
+﻿using CSC470_P3EmployeeInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestFakeEmployeeRepository
+{
+    public class GetSalary
+    {
+        public void GetSalaryWithGoodIdIsCorrect()
+        {
+            // Arrange
+            const int ID = 1;
+            const decimal EXPECTED_SALARY = (decimal)52435.69;
+            FakeEmployeeRepository EmpRepository = new FakeEmployeeRepository;
+            
+            // Act
+            decimal actualSalary = EmpRepository.GetSalary(ID);
+
+            //Assert
+            Assert.AreEqual(EXPECTED_SALARY, actualSalary);
+        }
+    }
+}
---
+++ CSC470-P3EmployeeInfo/UnitTestFakeEmployeeRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFakeEmployeeRepository")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFakeEmployeeRepository")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63f7041b-3365-481e-a438-86b21b6aaa8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
