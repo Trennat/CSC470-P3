7f6efbf99fac09bf9a2b1c844c4b8a0bb401eb06
Taner Tharp <tharptanner@gmail.com>
2020-09-17 21:34:43 -0500

    created Employee.cs

---
+++ CSC470-P3EmployeeInfo/CSC470-P3EmployeeInfo/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_P3EmployeeInfo
+{
+    public class Employee
+    {
+        public int id;
+        public string lastName;
+        public string firstName;
+        public decimal salary;
+    }
+}
